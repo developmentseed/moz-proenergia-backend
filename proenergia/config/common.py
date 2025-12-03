bf2f7d4ff3a75ad62e3f60d194c0c3d33cf12314
Wille Marcel <wille.yyz@gmail.com>
2025-12-03 09:10:16 -0300

    Add Django Unfold

--- proenergia/config/common.py
+++ proenergia/config/common.py
@@ -9,6 +9,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class Common(Configuration):
     INSTALLED_APPS = (
+        # unfold
+        "unfold",  # before django.contrib.admin
+        "unfold.contrib.filters",  # optional, if special filters are needed
+        "unfold.contrib.forms",  # optional, if special form elements are needed
+        "unfold.contrib.inlines",  # optional, if special inlines are needed
+        "unfold.contrib.import_export",  # optional, if django-import-export package is used
+        "unfold.contrib.guardian",  # optional, if django-guardian package is used
+        "unfold.contrib.simple_history",  # optional, if django-simple-history package is used
+        "unfold.contrib.location_field",  # optional, if django-location-field package is used
+        "unfold.contrib.constance",  # optional, if django-constance package is used
         "django.contrib.admin",
         "django.contrib.gis",
         "django.contrib.auth",
@@ -193,9 +203,16 @@ class Common(Configuration):
         ),
         "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     }
+    # API Docs
     SPECTACULAR_SETTINGS = {
         "TITLE": "Mozambique PROENERGIA+ API Docs",
         "DESCRIPTION": "Mozambique PROENERGIA+ API Documentation",
         "VERSION": "1.0.0",
         "SERVE_INCLUDE_SCHEMA": False,
+    }
+    # Django Unfold
+    UNFOLD = {
+        "SITE_TITLE": "Mozambique PROENERGIA+",
+        "SITE_HEADER": "Mozambique PROENERGIA+",
+        "SITE_SUBHEADER": "Administration Interface",
     }
