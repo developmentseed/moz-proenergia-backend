266411c16f6338e5ea2f9ef1a5837d10f38bd529
Wille Marcel <wille.yyz@gmail.com>
2026-01-22 10:22:08 -0300

    Remove some models from the Django admin interface

--- proenergia/users/admin.py
+++ proenergia/users/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django_celery_beat.models import (
+    ClockedSchedule,
+    CrontabSchedule,
+    IntervalSchedule,
+    PeriodicTask,
+    SolarSchedule,
+)
+from django_celery_results.models import GroupResult, TaskResult
+from rest_framework.authtoken.models import TokenProxy
 from unfold.admin import ModelAdmin
 
 from .models import User
@@ -8,3 +17,14 @@ from .models import User
 @admin.register(User)
 class UserAdmin(UserAdmin, ModelAdmin):
     pass
+
+
+# Remove some models from the Admin interface
+admin.site.unregister(TokenProxy)
+admin.site.unregister(TaskResult)
+admin.site.unregister(GroupResult)
+admin.site.unregister(PeriodicTask)
+admin.site.unregister(ClockedSchedule)
+admin.site.unregister(CrontabSchedule)
+admin.site.unregister(IntervalSchedule)
+admin.site.unregister(SolarSchedule)
