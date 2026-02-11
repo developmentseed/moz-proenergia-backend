0e2bb6e1216676571a80fccd3f936c2b771c936a
Sanjay Bhangar <sanjay@developmentseed.org>
2026-02-11 18:46:03 +0530

    lintfix

--- proenergia/config/local.py
+++ proenergia/config/local.py
@@ -42,5 +42,6 @@ class Local(Common):
         CELERY_TASK_STORE_EAGER_RESULT = True
         CELERY_TASK_EAGER_PROPAGATES = True
 
+
 #    GDAL_LIBRARY_PATH = "/opt/homebrew/opt/gdal/lib/libgdal.dylib"
 #    GEOS_LIBRARY_PATH = "/opt/homebrew/opt/geos/lib/libgeos_c.dylib"
