e25c79e50bdc40761b5c3e1d869361ada0d9a6c2
Sanjay Bhangar <sanjay@developmentseed.org>
2026-02-12 16:08:45 +0530

    changes to local.py to disable debug toolbar while running tests

--- proenergia/config/local.py
+++ proenergia/config/local.py
@@ -11,31 +11,34 @@ class Local(Common):
     DEBUG = True
 
     # Testing
+    TESTING = "test" in sys.argv
+
     INSTALLED_APPS = Common.INSTALLED_APPS
-    INSTALLED_APPS += ("debug_toolbar",)
-
-    # Debug Toolbar Middleware
     MIDDLEWARE = Common.MIDDLEWARE
-    MIDDLEWARE = list(MIDDLEWARE)
-    MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
 
-    # Debug Toolbar Configuration
-    INTERNAL_IPS = [
-        "127.0.0.1",
-        "localhost",
-    ]
+    if not TESTING:
+        INSTALLED_APPS += ("debug_toolbar",)
 
-    DEBUG_TOOLBAR_CONFIG = {
-        "SHOW_TOOLBAR_CALLBACK": lambda request: True,
-        "SHOW_COLLAPSED": True,
-    }
+        # Debug Toolbar Middleware
+        MIDDLEWARE = list(MIDDLEWARE)
+        MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+
+        # Debug Toolbar Configuration
+        INTERNAL_IPS = [
+            "127.0.0.1",
+            "localhost",
+        ]
+
+        DEBUG_TOOLBAR_CONFIG = {
+            "SHOW_TOOLBAR_CALLBACK": lambda request: True,
+            "SHOW_COLLAPSED": True,
+        }
 
     # Mail
     EMAIL_HOST = "localhost"
     EMAIL_PORT = 1025
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-    TESTING = "test" in sys.argv
     if TESTING:
         MEDIA_ROOT = tempfile.mkdtemp()
         CELERY_TASK_ALWAYS_EAGER = True
