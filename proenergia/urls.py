776a33692f3caa2199a99e263fb5fd726032c14b
Wille Marcel <wille.yyz@gmail.com>
2026-03-04 11:55:27 -0300

    Redirect / to /api/v1/docs/

--- proenergia/urls.py
+++ proenergia/urls.py
@@ -40,7 +40,9 @@ urlpatterns = [
     ),
     # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
-    re_path(r"^$", RedirectView.as_view(url=reverse_lazy("api-root"), permanent=False)),
+    re_path(
+        r"^$", RedirectView.as_view(url=reverse_lazy("swagger-ui"), permanent=False)
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Debug Toolbar URLs (only in DEBUG mode)
