a749ceab2533dc223f17b24060b5825bd4e95de3
Sanjay Bhangar <sanjay@developmentseed.org>
2026-04-07 16:36:46 +0530

    comment out local over-ride for GDAL path

--- proenergia/config/local.py
+++ proenergia/config/local.py
@@ -58,5 +58,5 @@ class Local(Common):
         CELERY_TASK_EAGER_PROPAGATES = True
 
 
-    GDAL_LIBRARY_PATH = "/opt/homebrew/opt/gdal/lib/libgdal.dylib"
-    GEOS_LIBRARY_PATH = "/opt/homebrew/opt/geos/lib/libgeos_c.dylib"
+#    GDAL_LIBRARY_PATH = "/opt/homebrew/opt/gdal/lib/libgdal.dylib"
+#    GEOS_LIBRARY_PATH = "/opt/homebrew/opt/geos/lib/libgeos_c.dylib"
